8235547b36e05501f8cef5e0fc9906a11af78193
gayathri20029 <90793287+gayathri20029@users.noreply.github.com>
2023-02-10 08:35:07 +0530

    number of primes in a spedified range.java

---
+++ Day 4/number of primes in a specified range.java
@@ -0,0 +1,16 @@
+ int primeCount = 0;
+
+        while (input1 <= input2) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(input1); i++)
+                if (input1 % i == 0)
+                    isPrime = false;
+
+            if (isPrime)
+                primeCount++;
+
+            input1++;
+        }
+        return primeCount;
+    }
+}
